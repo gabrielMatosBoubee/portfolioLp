03c6049c3103ffa39f70ea718c70d1bb64a6329e
gabrielMatosBoubee <gm.boubee@gmail.com>
2022-12-20 13:13:34 -0300

    adição do menu hamburguer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,19 @@ import LinksOutside from './LinksOutside';
 function Header() {
     return (
         <header className={styles.Header} >
-          <h3>Boubee</h3>
+          <input id='menu' type='checkbox' className={styles.menu} />
+              <label htmlFor="menu">
+            <div className={styles.hamburgerLines}>
+                <span className={`${styles.line} ${styles.line1}`}></span>
+                <span className={`${styles.line} ${styles.line2}`}></span>
+                <span className={`${styles.line} ${styles.line3}`}></span>
+            </div>
+          </label>
+          <h3 className={styles.t}>Boubee</h3>
           <aside className={styles.links}>
+            <div className={styles.linksInside}>
           <Links />
+            </div>
           <LinksOutside />
           </aside>
         </header>
