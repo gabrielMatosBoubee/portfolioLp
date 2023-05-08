38d62c98828182b09d2a94f93b3e2b3c5c07c488
gabrielMatosBoubee <gm.boubee@gmail.com>
2023-05-08 16:09:36 -0300

    feat: add new svgs(Technologies)

--- src/icons/data.js
+++ src/icons/data.js
@@ -7,7 +7,24 @@ import css from './vscode-icons_file-type-css.svg';
 import html from './vscode-icons_file-type-html.svg';
 import javaScript from './vscode-icons_file-type-js-official.svg';
 import vscode from './vscode-icons_file-type-vscode.svg';
+import typeScript from './typeScript 1.svg'
+import sql from './sql-database-generic-svgrepo-com.svg'
+import mocha from './mocha-seeklogo.com.svg'
+import docker from './docker-svgrepo-com.svg'
+import nodeJS from './icons8-nodejs-30.svg'
+import jwt from './jwtio-json-web-token.svg'
+import chai from './chai-svgrepo-com.svg'
+import jest from './jest-seeklogo.com.svg'
+import sequelize from './sequelize-svgrepo-com.svg'
+import mongoDb from './mongodb-svgrepo-com.svg'
+import express from './icons8-express-js.svg'
+import mongose from './mongose 1.svg'
+import RTL from './testing-library.svg'
 
-const data = [html, css, javaScript, react, redux, git, vscode, github, bootstrap]
+const data = [
+    html, css, git, vscode, github, javaScript, typeScript, 
+    nodeJS, react, redux, bootstrap, sql,
+    docker, jwt, sequelize, mongoDb, express,
+    mongose, RTL, chai, mocha, jest]
 
 export default data;
